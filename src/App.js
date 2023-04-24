f3c3b211d138460f14dc009f2e12db399055edcb
Klement Doda <klement.doda@gmail.com>
2023-04-24 15:51:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+function PersonCard(props) {
+  const [age, setAge] = useState(props.age);
+
+  const handleBirthday = () => {
+    setAge(age + 1);
+  };
+
+  return (
+    <div>
+      <h2>{props.firstName} {props.lastName}</h2>
+      <p>Age: {age}</p>
+      <p>Hair Color: {props.hairColor}</p>
+      <button onClick={handleBirthday}>Happy Birthday!</button>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard 
+        firstName="Doe" 
+        lastName="Jane" 
+        age={45} 
+        hairColor="Black" 
+      />
+      <PersonCard 
+        firstName="Smith" 
+        lastName="John" 
+          age={88}
+        hairColor="Brown" 
+      />
+      <PersonCard 
+        firstName="Fillmore" 
+        lastName="Millard" 
+        age={50}
+        hairColor="Brown" 
+      />
+      <PersonCard 
+        firstName="Smith" 
+        lastName="Maria" 
+        age={62}
+        hairColor="Brown" 
+      />
     </div>
   );
 }
